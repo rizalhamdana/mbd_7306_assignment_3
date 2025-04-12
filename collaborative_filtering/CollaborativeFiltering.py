3cfa3381b782431de4ccae780aaae6b8417d81a3
Rizal Hamdan Arigusti <rizalhamdana21@gmail.com>
2025-04-12 17:55:10 +0930

    initial commit - creating dir structures for project

---
+++ collaborative_filtering/CollaborativeFiltering.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+
+class CollaborativeFiltering:
+    ### Put Your Code Here
+    def __init__():
+        return
---
+++ pattern_mining/PatternMining.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+
+class PatternMining:
+    ### Put Your Code Here
+    def __init__():
+        return
